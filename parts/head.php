cc7ad99eba3000422bab4e8b2fcbd062cf8cba9c
Arash <arash.gh80@yahoo.com>
2017-07-21 18:54:04 +0430

    bootstrap, jquery

--- parts/head.php
+++ parts/head.php
@@ -1,2 +1,6 @@
-<link rel = "stylesheet" type = " text/css" href = " style.css">
-		<meta charset = "utf-8">
+<link rel = "stylesheet" type = " text/css" href = " css/style.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    
+<meta charset = "utf-8">
